56cdb21a27c47dd3cb7258d4c3be958da5eeeacf
Zachary P. Christensen <zchristensen7@gmail.com>
2020-02-29 02:54:51 -0500

    Generalize broadcasting
    Broadcasting should work on AbstractAxisIndices now (as opposed to just
    AxisIndicesArray)

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -23,7 +23,6 @@ function Base.BroadcastStyle(::Type{T}) where {T<:AxisIndicesArray}
     return AxisIndicesArrayStyle{typeof(BroadcastStyle(parent_type(T)))}()
 end
 
-
 Base.BroadcastStyle(::AxisIndicesArrayStyle{A}, ::AxisIndicesArrayStyle{B}) where {A, B} = AxisIndicesArrayStyle(A(), B())
 Base.BroadcastStyle(::AxisIndicesArrayStyle{A}, b::B) where {A, B} = AxisIndicesArrayStyle(A(), b)
 Base.BroadcastStyle(a::A, ::AxisIndicesArrayStyle{B}) where {A, B} = AxisIndicesArrayStyle(a, B())
@@ -33,29 +32,37 @@ Base.BroadcastStyle(a::AbstractArrayStyle{M}, ::AxisIndicesArrayStyle{B}) where 
 """
     unwrap_broadcasted
 
-Recursively unwraps `AxisIndicesArray`s and `AxisIndicesArrayStyle`s.
-replacing the `AxisIndicesArray`s with the wrapped array,
+Recursively unwraps `AbstractAxisIndices`s and `AxisIndicesArrayStyle`s.
+replacing the `AbstractAxisIndices`s with the wrapped array,
 and `AxisIndicesArrayStyle` with the wrapped `BroadcastStyle`.
 """
 function unwrap_broadcasted(bc::Broadcasted{AxisIndicesArrayStyle{S}}) where S
     return Broadcasted{S}(bc.f, map(unwrap_broadcasted, bc.args))
 end
-#unwrap_broadcasted(bc::Broadcasted{BroadcastIndexStyle{S}}) where S = Broadcasted{S}(bc.f, map(unwrap_broadcasted, bc.args))
-unwrap_broadcasted(a::AxisIndicesArray) = parent(a)
-#unwrap_broadcasted(a::AbstractIndex) = parent(a)
+unwrap_broadcasted(a::AbstractAxisIndices) = parent(a)
 unwrap_broadcasted(x) = x
+
+get_first_axis_indices(bc::Broadcasted) = _get_first_axis_indices(bc.args)
+_get_first_axis_indices(args::Tuple{Any,Vararg{Any}}) = _get_first_axis_indices(tail(args))
+_get_first_axis_indices(args::Tuple{<:AbstractAxisIndices,Vararg{Any}}) = first(args)
+_get_first_axis_indices(args::Tuple{}) = nothing
 
 # We need to implement copy because if the wrapper array type does not support setindex
 # then the `similar` based default method will not work
-# TODO: need to make this flexible to other AxisIndicesArrays types
 function Broadcast.copy(bc::Broadcasted{AxisIndicesArrayStyle{S}}) where S
-    return AxisIndicesArray(Broadcast.copy(unwrap_broadcasted(bc)),
-                     Broadcast.combine_axes(bc.args...))
+    return _similar_type(get_first_axis_indices(bc),
+                         Broadcast.copy(unwrap_broadcasted(bc)),
+                         Broadcast.combine_axes(bc.args...))
 end
 
 function Base.copyto!(dest::AbstractArray, bc::Broadcasted{AxisIndicesArrayStyle{S}}) where S
     inner_bc = unwrap_broadcasted(bc)
     copyto!(dest, inner_bc)
-    return AxisIndicesArray(dest)
+    _similar_type(get_first_axis_indices(bc), dest)
 end
-# TODO make the above generic
+
+# catch cases where get_first_axis_indices couldn't find a AbstractAxisIndices
+function _similar_type(A::AbstractAxisIndices, p, axs=axes(A))
+    return similar_type(A, typeof(p), typeof(axs))(p, axs)
+end
+_similar_type(::Nothing, p, axs=nothing) = AxisIndicesArray(p)
