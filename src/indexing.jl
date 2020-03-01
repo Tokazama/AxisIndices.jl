d920962f4adbcd2e0ce346b75742b56de9ef0b6a
Zachary P. Christensen <zchristensen7@gmail.com>
2020-03-01 02:14:45 -0500

    Test `filter` as expected

--- src/indexing.jl
+++ src/indexing.jl
@@ -8,18 +8,21 @@ for f in (:getindex, :view, :dotview)
     _f = Symbol(:_, f)
     @eval begin
         @propagate_inbounds function Base.$f(a::AbstractAxisIndices, inds...)
-            return $_f(a, to_indices(a, axes(a), inds)...)
+            return $_f(a, to_indices(a, axes(a), inds))
         end
 
-        @propagate_inbounds function $_f(a::AbstractAxisIndices, inds::Vararg{<:Integer})
+        @propagate_inbounds function $_f(a::AbstractAxisIndices, inds::Tuple{Vararg{<:Integer}})
             return Base.$f(parent(a), inds...)
         end
 
-        @propagate_inbounds function $_f(a::AbstractAxisIndices, ci::CartesianIndex)
-            return Base.$f(parent(a), ci)
-        end
+        #@propagate_inbounds function $_f(a::AbstractAxisIndices, ci::CartesianIndex)
+        #    return Base.$f(parent(a), ci)
+        #end
 
-        @propagate_inbounds function $_f(a::AbstractAxisIndices, inds...)
+        @propagate_inbounds function $_f(a::AbstractAxisIndices{T,N}, inds::Tuple{Vararg{<:Any,M}}) where {T,N,M}
+            return Base.$f(parent(a), inds...)
+        end
+        @propagate_inbounds function $_f(a::AbstractAxisIndices{T,N}, inds::Tuple{Vararg{<:Any,N}}) where {T,N}
             p = Base.$f(parent(a), inds...)
             axs = reindex(axes(a), inds)
             return similar_type(a, typeof(p), typeof(axs))(p, axs)
--- test/AxisInterface/runtests.jl
+++ test/AxisInterface/runtests.jl
@@ -75,4 +75,11 @@ include("broadcast_tests.jl")
     end
 end
 
+@testset "filter" begin
+    a = AxisIndicesArray([11 12; 21 22], (2:3, 3:4))
+    v = AxisIndicesArray(1:7, (2:8,))
+
+    @test axes_keys(filter(isodd, v)) == ([2, 4, 6, 8],)
+    @test axes_keys(filter(isodd, a)) == (1:2,)
+end
 
