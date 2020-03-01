0d6200f0f1647a576a8ea48e9cf8e2acb78615aa
Zachary P. Christensen <zchristensen7@gmail.com>
2020-03-01 00:51:35 -0500

    Add eigen method for Julia v1.2

--- src/LinearAlgebra/eigen.jl
+++ src/LinearAlgebra/eigen.jl
@@ -1,4 +1,6 @@
 
+LinearAlgebra.eigen(A::AbstractAxisIndices; kwargs...) = LinearAlgebra.eigen!(A; kwargs...)
+
 function LinearAlgebra.eigen!(A::AbstractAxisIndices{T,N,P,AI}; kwargs...) where {T,N,P,AI}
     vals, vecs = LinearAlgebra.eigen!(parent(A); kwargs...)
     return Eigen(vals, similar_type(A, typeof(vecs), AI)(vecs, axes(A)))
