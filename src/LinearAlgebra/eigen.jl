e5628e3f830c3b476dc1731579c149490e4851eb
Zachary P. Christensen <zchristensen7@gmail.com>
2020-03-01 01:05:41 -0500

    Okay, this should work on v1.2

--- src/LinearAlgebra/eigen.jl
+++ src/LinearAlgebra/eigen.jl
@@ -1,9 +1,16 @@
 
-LinearAlgebra.eigen(A::AbstractAxisIndices; kwargs...) = LinearAlgebra.eigen!(A; kwargs...)
+function LinearAlgebra.eigen(A::AbstractAxisIndices{T,N,P,AI}; kwargs...) where {T,N,P,AI}
+    vals, vecs = LinearAlgebra.eigen(parent(A); kwargs...)
+    return Eigen(vals, similar_type(A, typeof(vecs), AI)(vecs, axes(A)))
+end
 
 function LinearAlgebra.eigen!(A::AbstractAxisIndices{T,N,P,AI}; kwargs...) where {T,N,P,AI}
     vals, vecs = LinearAlgebra.eigen!(parent(A); kwargs...)
     return Eigen(vals, similar_type(A, typeof(vecs), AI)(vecs, axes(A)))
+end
+
+function LinearAlgebra.eigvals(A::AbstractAxisIndices; kwargs...)
+    return LinearAlgebra.eigvals(parent(A); kwargs...)
 end
 
 function LinearAlgebra.eigvals!(A::AbstractAxisIndices; kwargs...)
