9544ac0f8dfa41eefde6037c68b41fa7f5991e8f
Tokazama <zchristensen@gmail.com>
2020-04-25 10:03:34 -0400

    Simplify to_axes
    This was too complex and required code generation to avoid ambiguities.
    Now it's just one slightly messier method instead of making a bunch of
    extra useless code.

--- src/AxisCore/to_axes.jl
+++ src/AxisCore/to_axes.jl
@@ -4,95 +4,61 @@
     args::Tuple{T, Vararg{Any}},
     inds::Tuple,
     new_indices::Tuple{Vararg{Any}}
-) where {A<:AbstractAxis,T}
+) where {A,T}
 
     S = AxisIndicesStyle(A, T)
     if is_element(S)
-        to_axes(
+        return to_axes(
             maybetail(old_axes),
             maybetail(args),
             maybetail(inds),
             new_indices,
         )
     else
-        axis = first(old_axes)
-        return (
-            similar(
-                axis,
-                to_keys(S, axis, first(args), first(inds)),
-                first(new_indices),
-                false
-            ),
-            to_axes(
-                maybetail(old_axes),
-                maybetail(args),
-                maybetail(inds),
-                maybetail(new_indices)
-            )...
+        return (if is_simple_axis(A)
+                    similar(first(old_axes), first(new_indices))
+                else
+                    axis = first(old_axes)
+                    similar(axis, to_keys(S, axis, first(args), first(inds)), first(new_indices), false)
+                end,
+                to_axes(
+                    maybetail(old_axes),
+                    maybetail(args),
+                    maybetail(inds),
+                    maybetail(new_indices)
+                )...
         )
     end
 end
 
 @inline function to_axes(
     old_axes::Tuple{A,Vararg{Any}},
-    args::Tuple{T, Vararg{Any}},
+    args::Tuple{CartesianIndex{N},Vararg{Any}},
     inds::Tuple,
-    new_indices::Tuple{Vararg{Any}}
-) where {A<:AbstractSimpleAxis,T}
+    new_axes::Tuple{Vararg{Any}},
+) where {A,N}
 
-    S = AxisIndicesStyle(A, T)
-    if is_element(S)
-        to_axes(
-            maybetail(old_axes),
-            maybetail(args),
-            maybetail(inds),
-            new_indices,
-        )
-    else
-        return (
-            similar(first(old_axes), first(new_indices)),
-            to_axes(
-                maybetail(old_axes),
-                maybetail(args),
-                maybetail(inds),
-                maybetail(new_indices)
-            )...
-        )
-    end
+    _, old_axes2 = Base.IteratorsMD.split(old_axes, Val(N))
+    _, inds2 = Base.IteratorsMD.split(inds, Val(N))
+    return to_axes(old_axes2, tail(args), inds2, new_axes)
 end
 
-for T in (AbstractAxis,AbstractSimpleAxis)
-    @eval begin
-        @inline function to_axes(
-            old_axes::Tuple{A,Vararg{Any}},
-            args::Tuple{CartesianIndex{N},Vararg{Any}},
-            inds::Tuple,
-            new_axes::Tuple{Vararg{Any}},
-        ) where {A<:$T,N}
+@inline function to_axes(
+    old_axes::Tuple{A,Vararg{Any}},
+    args::Tuple{CartesianIndices, Vararg{Any}},
+    inds::Tuple,
+    new_axes::Tuple{Vararg{Any}}
+) where {A}
 
-            _, old_axes2 = Base.IteratorsMD.split(old_axes, Val(N))
-            _, inds2 = Base.IteratorsMD.split(inds, Val(N))
-            return to_axes(old_axes2, tail(args), inds2, new_axes)
-        end
+    return to_axes(old_axes, tail(args), tail(inds), new_axes)
+end
 
-        @inline function to_axes(
-            old_axes::Tuple{A,Vararg{Any}},
-            args::Tuple{CartesianIndices, Vararg{Any}},
-            inds::Tuple,
-            new_axes::Tuple{Vararg{Any}}
-        ) where {A<:$T}
+function to_axes(::Tuple{A,Vararg{Any}}, ::Tuple{CartesianIndex{N},Vararg{Any}}, ::Tuple, ::Tuple{}) where {A,N}
+    return ()
+end
 
-            return to_axes(old_axes, tail(args), tail(inds), new_axes)
-        end
-
-        function to_axes(::Tuple{A,Vararg{Any}}, ::Tuple{CartesianIndex{N},Vararg{Any}}, ::Tuple, ::Tuple{}) where {A<:$T,N}
-            return ()
-        end
-
-        function to_axes(::Tuple{A,Vararg{Any}}, ::Tuple{Any,Vararg{Any}}, ::Tuple, ::Tuple{}) where {A<:$T}
-            return ()
-        end
-    end
+function to_axes(::Tuple{A,Vararg{Any}}, ::Tuple{Any,Vararg{Any}}, ::Tuple, ::Tuple{}) where {A}
+    return ()
 end
 
 to_axes(::Tuple{}, ::Tuple, ::Tuple, ::Tuple{}) = ()
