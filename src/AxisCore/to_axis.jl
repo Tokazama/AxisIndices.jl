b805b4bcef74a84556a20cee8ceb0c7ecf99bf56
Zachary P. Christensen <zchristensen7@gmail.com>
2020-04-30 02:23:30 -0400

    to_axis has own file now

--- src/AxisCore/to_axis.jl
+++ src/AxisCore/to_axis.jl
@@ -9,4 +9,3 @@ to_axis(axis::AbstractUnitRange{<:Integer}) = SimpleAxis(axis)
 to_axis(ks::AbstractVector) = Axis(ks)
 to_axis(ks::AbstractVector, vs::AbstractUnitRange{<:Integer}) = Axis(ks, vs)
 to_axis(len::Integer) = SimpleAxis(len)
-
