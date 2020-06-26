e637aff568842dc563ace1658422ad61914f14a6
Zachary P. Christensen <zchristensen7@gmail.com>
2020-06-26 07:56:39 -0400

    Default metadata for MetaAxis is Dict{Symbol,Any}

--- test/Axes/MetaAxis.jl
+++ test/Axes/MetaAxis.jl
@@ -10,27 +10,26 @@
     @test @inferred(meta_axis[:a]) == 1
     @test @inferred(has_metadata(meta_axis))
     @test @inferred(has_metadata(typeof(meta_axis)))
-    @test @inferred(metadata(meta_axis)) isa Nothing
-    @test @inferred(metadata_type(meta_axis)) <: Nothing
-    @test @inferred(metadata_type(typeof(meta_axis))) <: Nothing
+    @test @inferred(metadata(meta_axis)) isa Dict
+    @test @inferred(metadata_type(meta_axis)) <: Dict{Symbol,Any}
+    @test @inferred(metadata_type(typeof(meta_axis))) <: Dict{Symbol,Any}
     @test !@inferred(is_indices_axis(meta_axis))
     @test !@inferred(is_indices_axis(typeof(meta_axis)))
 
 
-    meta_axis2 = MetaAxis(1:2, Dict())
+    meta_axis2 = MetaAxis(1:2, nothing)
     @test Interface.combine_metadata(meta_axis, meta_axis2) isa Dict
     @test Interface.combine_metadata(meta_axis2, meta_axis) isa Dict
-    @test Interface.combine_metadata(meta_axis2, meta_axis2) isa Dict
-    @test Interface.combine_metadata(meta_axis, meta_axis) isa Nothing
+    @test Interface.combine_metadata(meta_axis2, meta_axis2) isa Nothing
+    @test Interface.combine_metadata(meta_axis, meta_axis) isa Dict
     @test Interface.combine_metadata(1, nothing) isa Int
     @test Interface.combine_metadata(nothing, 1) isa Int
 
-    @test metadata(view(meta_axis2, 1:2)) isa Dict
+    @test metadata(view(meta_axis2, 1:2)) isa Nothing
 
-    A = AxisArray(ones(2,2), meta_axis2, 1:2)
+    A = AxisArray(ones(2,2), meta_axis, 1:2)
     @test typeof(axis_meta(A)) <: Tuple{<:Dict,Nothing}
     @test typeof(axis_meta(A, 1)) <: Dict
 
     @test metadata(MetaAxis([:a, :b], 1:2, 1)) isa Int
-
 end
